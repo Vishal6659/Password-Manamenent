bf7983f5bd174f1c71d3175ce15c1fafad2531d4
Vishal pal <vishal@contaque.com>
2023-08-03 17:30:43 +0530

    First Commit - NAvbar created using Tailwind CSS

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,11 @@
+export const enviroment = {
+    //production: true
+    firebase: {
+        projectId: 'password-manager-18f59',
+        appId: '1:292517226317:web:1fb11366815fba4dda3904',
+        storageBucket: 'password-manager-18f59.appspot.com',
+        apiKey: 'AIzaSyAKBhSsTBzwDRN6NP3QWtHV9AbgDMkmuCY',
+        authDomain: 'password-manager-18f59.firebaseapp.com',
+        messagingSenderId: '292517226317',
+      },
+}
