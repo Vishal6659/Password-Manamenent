afe04c15b2499430f6c60d07c17cff792845bfb5
Vishal pal <vishal@contaque.com>
2023-08-04 14:00:50 +0530

    Site-List component Completed

--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PasswordManagerService } from '../password-manager.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-site-list',
@@ -8,17 +9,70 @@ import { PasswordManagerService } from '../password-manager.service';
 })
 export class SiteListComponent {
 
-  constructor(private passwordManager: PasswordManagerService){}
+  allSites !: Observable<Array<any>>;
+
+  siteName !: string;
+  siteUrl !: string;
+  siteImgUrl !: string;
+  siteId !: string;
+
+  formState: string = "Add New";
+
+  isSuccess: boolean = false;
+  successMessage !: string;
+
+  constructor(private passwordManagerService: PasswordManagerService){
+    this.loadSites();
+  }
+
+  showAlert(message: string){
+    this.isSuccess = true;
+    this.successMessage = message;
+  }
 
   onSubmit(values: object){
-    console.log(values);
-    this.passwordManager.addSite(values)
+    if(this.formState == "Add New"){
+      this.passwordManagerService.addSite(values)
     .then(()=>{
-      console.log('Data Saved Succesfully ')
+      this.showAlert('Data Saved Succesfully');
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+    } 
+    else if(this.formState = "Edit"){
+      this.passwordManagerService.updateSite(this.siteId, values)
+      .then(()=>{
+        this.showAlert('Data Edit Succesfully');
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }     
+  }
+
+  loadSites(){
+   this.allSites =  this.passwordManagerService.loadSites();
+  }
+
+  editSite(siteName: string, siteUrl: string, siteImgUrl: string, id: string){
+    this.siteName = siteName;
+    this.siteUrl = siteUrl;
+    this.siteImgUrl = siteImgUrl;
+    this.siteId = id;
+    this.formState = "Edit";
+  }
+
+  deleteSite(id:string){
+    this.passwordManagerService.deleteSite(id)
+    .then(()=>{
+      this.showAlert('Data Deleted Succesfully');
     })
     .catch(err=>{
       console.log(err);
     })
   }
+
+
 
 }
