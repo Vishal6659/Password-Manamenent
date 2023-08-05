0af2707983fb8378a6a0a4a3e0a07cf215dc3246
Vishal pal <vishal@contaque.com>
2023-08-06 04:46:58 +0530

    Every other work is done except In Add Password Functionality -> Decrypt button is not working properly

--- src/app/password-manager.service.ts
+++ src/app/password-manager.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collection, addDoc, collectionData, doc, updateDoc, deleteDoc } from '@angular/fire/firestore';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PasswordManagerService {
 
-  constructor(private firestore: Firestore) { }
+  constructor(private firestore: Firestore, private auth: Auth) { }
 
   addSite(data: object){
     const dbInstance = collection(this.firestore, 'sites');
@@ -26,5 +27,31 @@ export class PasswordManagerService {
   deleteSite(id: string){
     const docInstance = doc(this.firestore, 'sites', id);
     return deleteDoc(docInstance);
+  }
+
+  // Password Queries
+  addPassword(data: object, siteId: string){
+    const dbInstance = collection(this.firestore, `sites/${siteId}/passwords`);
+    return addDoc(dbInstance, data);
+  }
+
+  loadPasswords(siteId: string){
+    const dbInstance = collection(this.firestore, `sites/${siteId}/passwords`);
+    return collectionData(dbInstance, {idField: 'id'});
+  }
+
+  updatePassword(siteId: string, passwordId:string, data: object){
+    const docInstant = doc(this.firestore, `sites/${siteId}/passwords`, passwordId);
+    return updateDoc(docInstant, data);
+  }
+
+  deletePassword(siteId: string, passwordId: string){
+    const docInstant = doc(this.firestore, `sites/${siteId}/passwords`, passwordId);
+    return deleteDoc(docInstant);
+  }
+
+  // Login
+  login(emain: string, password: string){
+    return signInWithEmailAndPassword(this.auth, emain, password);
   }
 }
